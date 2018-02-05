0a42d95cdd9a621ab4dbe5198a0dc02352898b25
RobbyTan <robbytanrotan@gmail.com>
2018-02-05 20:46:09 +0700

    fixing index routes sonder to startup

--- models/user.js
+++ models/user.js
@@ -2,11 +2,14 @@ var mongoose   = require('mongoose');
 var passportLocalMongoose = require("passport-local-mongoose");
 var userSchema = new mongoose.Schema({
 	username : String,
-	email : String,
+	email : {type:String, unique: true, required:true},
 	birthday: String,
 	gender : String,
+	status : String,
 	image : String,
-	password : String
+	password : String,
+	resetPasswordToken: String,
+	resetPasswordExpires: Date
 })
 
 userSchema.plugin(passportLocalMongoose);
